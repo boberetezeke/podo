aebe3a43afd9c1393367f9959033ba50a0a1a029
Steve Tuckner <steve@open-orgsolutions.com>
2014-01-14 11:01:20 -0600

    Initial Commit

---
+++ PoDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PoDo
+//
+//  Created by Steve Tuckner on 1/14/14.
+//  Copyright (c) 2014 Steve Tuckner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PoDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PoDo
+//
+//  Created by Steve Tuckner on 1/14/14.
+//  Copyright (c) 2014 Steve Tuckner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
